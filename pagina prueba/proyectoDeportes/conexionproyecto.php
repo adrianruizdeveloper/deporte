05176d4165b8b6bed761c71659b8ab371a906bde
joseangellopez <43651769+joseangellopez@users.noreply.github.com>
2019-11-25 09:36:44 +0100

    Add files via upload

---
+++ pagina prueba/proyectoDeportes/conexionproyecto.php
@@ -0,0 +1,14 @@
+<?php
+
+try {
+      $user = "root";  // usuario con el que se va conectar con MySQL
+      $pass = "";  // contraseña del usuario 
+      $dbname = "proyecto";  //nombre de la base de datos
+      $db = new PDO("mysql:host=localhost; dbname=$dbname", $user, $pass);  //conectar con MySQL y SELECCIONAR LA BBDD 
+      $db = null;  // cerrar la conexión
+    } catch (PDOException $e) {  // Si hubieran errores de conexión, se captura un objeto de tipo PDOException
+        print "<p>Error: NO SE PUDO CONECTAR AL SERVIDOR MySQL o a la BASE DE DATOS.</p>\n";
+        print "<p>Error: " . $e->getMessage() . "</p>\n";  // mensaje de excepción
+        exit();
+    }
+?>
