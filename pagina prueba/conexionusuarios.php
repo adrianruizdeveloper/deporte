01a98499f268ff5c2d64a97ed43191f371e5aad1
joseangellopez <43651769+joseangellopez@users.noreply.github.com>
2019-11-26 10:29:51 +0100

    Add files via upload

--- pagina prueba/conexionusuarios.php
+++ pagina prueba/conexionusuarios.php
@@ -5,7 +5,8 @@ try {
       $pass = "";  // contraseña del usuario 
       $dbname = "usuarios";  //nombre de la base de datos
       $db = new PDO("mysql:host=localhost; dbname=$dbname", $user, $pass);  //conectar con MySQL y SELECCIONAR LA BBDD 
-      $db = null;  // cerrar la conexión
+    $db->exec("set names utf8");
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     } catch (PDOException $e) {  // Si hubieran errores de conexión, se captura un objeto de tipo PDOException
         print "<p>Error: NO SE PUDO CONECTAR AL SERVIDOR MySQL o a la BASE DE DATOS.</p>\n";
         print "<p>Error: " . $e->getMessage() . "</p>\n";  // mensaje de excepción
