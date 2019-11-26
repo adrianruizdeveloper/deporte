51217c56a1e043d48f75325bde1649607068ac8b
joseangellopez <43651769+joseangellopez@users.noreply.github.com>
2019-11-26 11:53:33 +0100

    Add files via upload

--- pagina prueba/calendario.php
+++ pagina prueba/calendario.php
@@ -6,7 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <title>calendario</title>
     <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
     <link rel="stylesheet" href="assets/css/styles_calendario.css">
      <!-- Cabecera y pie-->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Cookie">
@@ -16,7 +15,6 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700">
     <link rel="stylesheet" href="assets/css/Footer-Dark.css">
     <link rel="stylesheet" href="assets/css/Header-Blue.css">
-    <link rel="stylesheet" href="assets/css/styles.css">
      <link rel="stylesheet" href="assets/css/Navigation-with-Button_cabecera.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <?php include 'conexionproyecto.php';
