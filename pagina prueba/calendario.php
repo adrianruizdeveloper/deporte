b13fa8e1912951ee608eaafe7582dc0708ffd773
joseangellopez <43651769+joseangellopez@users.noreply.github.com>
2019-11-26 10:53:34 +0100

    Add files via upload

--- pagina prueba/calendario.php
+++ pagina prueba/calendario.php
@@ -18,13 +18,16 @@
     <link rel="stylesheet" href="assets/css/Header-Blue.css">
     <link rel="stylesheet" href="assets/css/styles.css">
      <link rel="stylesheet" href="assets/css/Navigation-with-Button_cabecera.css">
-    
+        <?php include 'conexionproyecto.php';
+    $consulta="SELECT idpartido, fecha_cal,local_cal,goleslocal_cal,visitante_cal,golesvisitante_cal,idestadio_cal FROM calendario";
+
+    ?>
+
 
 </head>
 
 <body>
                 <?php include 'cabecera.php'; ?>
-    <?php include 'conexionproyecto.php'; ?>
 
     <div class="table-responsive" id="calendario">
         <table class="table">
@@ -41,21 +44,70 @@
                 </tr>
             </thead>
             <tbody>
-                <tr>
-                    <td>1</td>
-                    <td>21/11/2019</td>
-                    <td>Villacarrillo</td>
-                    <td>0</td>
-                    <td>VS</td>
-                    <td>9</td>
-                    <td>Villanueva del arzobispo</td>
-                    <td>La Victoria</td>
+
+
+            <?php
+
+            // Se crea un bucle que se repetirá tantas veces como filas tenga la consulta.
+            foreach ($db->query($consulta) as $fila) {
+                $Jornada = $fila['idpartido'];
+                $Fecha = $fila['fecha_cal'];
+                $Local = $fila['local_cal'];
+                $GolesL = $fila['goleslocal_cal'];
+                $GolesV = $fila['golesvisitante_cal'];
+                $Visitante = $fila['visitante_cal'];
+                $Estadio = $fila['idestadio_cal'];
+            //$localvisitante = "Select nombre_eq from equipo where idequipo=".$equipo.";";
+
+                    /*function localVisitante($equipo){
+                        foreach ($db->query($localvisitante) as $fila) {
+                        $hola = $fila['nombre_eq'];
+
+                    }
+                    return $hola;
+                }*/
+                $consulta2 = "Select nombre_eq from equipo where idequipo=".$Local.";";
+                //$consulta2 = "Select nombre_eq from equipo where idequipo=".localvisitante($Local).";";
+                $consulta3 = "Select nombre_eq from equipo where idequipo=".$Visitante.";";
+                //$consulta3 = "Select nombre_eq from equipo where idequipo=".localvisitante($Visitante).";";
+                $consulta4 = "Select * from estadio where idestadio=".$Estadio.";";
+                foreach ($db->query($consulta2) as $fila) {
+                   $Local2 = $fila['nombre_eq'];
+
+                }
+                foreach ($db->query($consulta3) as $fila) {
+                   $Vistante2 = $fila['nombre_eq'];
+
+                }
+                foreach ($db->query($consulta4) as $fila) {
+                    $estadio2 = $fila['nombre_estadio'];
+
+                }
+                ?>
+                <!-- Se crea una fila de la tabla con los datos obtenidos -->
+                <tr><td><?php echo ($Jornada); ?></td>
+                    <td><?php echo ($Fecha); ?></td>
+                    <td><?php echo ($Local2); ?></td>
+                    <td><?php echo ($GolesL); ?></td>
+                    <td><?php echo ("VS"); ?></td>
+                    <td><?php echo ($GolesV); ?></td>
+                    <td><?php echo ($Vistante2); ?></td>
+                    <td><?php echo ($estadio2); ?></td>
+
                 </tr>
+                <?php
+            }
+            ?>
             </tbody>
         </table>
     </div>
+
+
            <?php include 'pie.php'; ?>
 
 </body>
 
 </html>
+
+
+
