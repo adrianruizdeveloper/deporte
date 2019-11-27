a5e98d7a8a89b24d7266d4fd6ed57cb176388c30
adrianruizmartinez <43651863+adrianruizmartinez@users.noreply.github.com>
2019-11-27 10:04:44 +0100

    Add files via upload

--- pagina prueba/jugadores.php
+++ pagina prueba/jugadores.php
@@ -2,11 +2,10 @@
 <html>
 
 <head>
+
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <title>jugadores</title>
-    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="assets/css/styles_jugadores.css">
     <!-- Cabecera y pie-->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Cookie">
     <link rel="stylesheet" href="assets/css/Navigation-with-Button.css">
@@ -16,13 +15,20 @@
     <link rel="stylesheet" href="assets/css/Footer-Dark.css">
     <link rel="stylesheet" href="assets/css/Header-Blue.css">
     <link rel="stylesheet" href="assets/css/Navigation-with-Button_cabecera.css">
+
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/styles_jugadores.css">
+
     <style>
-        img {
+        .img_jug {
             margin-left: 0px;
         }
     </style>
 </head>
+<script >
 
+
+</script>
 <body>
 <?php include 'cabecera.php'; ?>
 <?php include 'conexionproyecto.php'; ?>
@@ -30,18 +36,24 @@
 //Lista de equipos
 $lista = "";
 $consulta_equipos = "SELECT idequipo, nombre_eq from equipo;";
+$url = $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"];
 foreach ($db->query($consulta_equipos) as $fila) {
     $idequipo = $fila['idequipo'];
     $nombre_eq = $fila['nombre_eq'];
-    $lista .= "<li value=\"" . $idequipo . "\">" . $nombre_eq . "</li>";
+    $lista .= "<li value=\"" . $idequipo . "\"><a href=?equipo=" . $idequipo . ">". $nombre_eq ."</a></li>";
 }
 //_-----------------------------------------------------------------------_\\
-$jugadores = "";
-$consulta_jugadores = "SELECT idjugador, alias_jug from jugador";
-foreach ($db->query($consulta_jugadores) as $fila) {
-    $idjugador = $fila['idjugador'];
-    $alias_jug = $fila['alias_jug'];
-    $jugadores .= "<option value=\"" . $idjugador . "\">" . $alias_jug . "</option>";
+if(isset($_GET['equipo'])){
+    $jugadores = "";
+    $equipo = (int)$_GET['equipo'];
+    $consulta_jugadores = "SELECT idjugador, alias_jug , equipos_jug from jugador where equipos_jug=". $equipo;
+    foreach ($db->query($consulta_jugadores) as $fila) {
+        $idjugador = $fila['idjugador'];
+        $alias_jug = $fila['alias_jug'];
+        $jugadores .= "<option value=\"" . $idjugador . "\">" . $alias_jug . "</option>";
+    }
+}else {
+
 }
 //_----------------------------------------------------------_\\
 $info_jug = "";
@@ -83,7 +95,7 @@ foreach ($db->query($consulta_equipo_jug) as $fila) {
                 </select>
                 <div class="table-responsive">
                     <table class="table">
-                        <img src="assets/img/descarga.png"/>
+                        <img class="img_jug" src="assets/img/descarga.png"/>
                         <p>Posición:</p>
                         <p><?php echo $idposicion_jug ?></p>
                         <thead>
