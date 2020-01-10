9d92114eaf5fb4ed191fd868d0cea58660dd9896
adrianruizmartinez <43651863+adrianruizmartinez@users.noreply.github.com>
2020-01-10 12:18:28 +0100

    Add files via upload

--- pagina prueba/conexionusuarios.php
+++ pagina prueba/conexionusuarios.php
@@ -4,7 +4,7 @@ try {
       $user = "root";  // usuario con el que se va conectar con MySQL
       $pass = "";  // contraseña del usuario 
       $dbname = "usuarios";  //nombre de la base de datos
-      $db2 = new PDO("mysql:host=localhost; dbname=$dbname", $user, $pass);  //conectar con MySQL y SELECCIONAR LA BBDD 
+      $db2 = new PDO("mysql:host=localhost; dbname=$dbname", $user, $pass);  //conectar con MySQL y SELECCIONAR LA BBDD
     $db2->exec("set names utf8");
     $db2->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $db2->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
