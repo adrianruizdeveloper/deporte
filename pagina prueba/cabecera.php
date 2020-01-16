f3e35259d5015a9f4ebabd8626eaada86a750594
Jose Angel López Santos <joseangelmolina27@hotmail.com>
2020-01-16 09:58:46 +0000

    origin

--- pagina prueba/cabecera.php
+++ pagina prueba/cabecera.php
@@ -1,7 +1,7 @@
 <?php
 include 'conexionusuarios.php';
 $menu = "";
-//prueba
+//prueba2
 if (!isset($_COOKIE['id_usuario'])) {
     //$login_logout = '<li><a href="login.php">Login</a></li>
     //                <li><a href="registro.php">Register</li>';
