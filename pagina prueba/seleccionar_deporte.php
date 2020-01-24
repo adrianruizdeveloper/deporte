6b8e788e63b11e63b790e0edbe2f3335326c8b26
adrianruizmartinez <43651863+adrianruizmartinez@users.noreply.github.com>
2020-01-24 11:59:29 +0100

    Add files via upload

--- pagina prueba/seleccionar_deporte.php
+++ pagina prueba/seleccionar_deporte.php
@@ -5,19 +5,23 @@ session_start();
 
 switch ($deporte) {
 case 1:
-$_SESSION['deporte'] = "1";
+$_SESSION['deporte'] = 1;
+setcookie('deporte', $_SESSION['deporte']);
 echo 'futbol';
 break;
 case 2:
-$_SESSION['deporte'] = "2";
+$_SESSION['deporte'] = 2;
+setcookie('deporte', $_SESSION['deporte']);
 echo 'futbol sala';
 break;
 case 3:
-$_SESSION['deporte'] = "3";
+$_SESSION['deporte'] = 3;
+setcookie('deporte', $_SESSION['deporte']);
 echo 'baloncesto';
 break;
 case 4:
-$_SESSION['deporte'] = "4";
+$_SESSION['deporte'] = 4;
+    setcookie('deporte', $_SESSION['deporte']);
 echo 'balonmano';
 break;
 }
