3cc88cea03fa8411b6aadf89047dc78e30c3504e
joseangellopez <43651769+joseangellopez@users.noreply.github.com>
2020-03-09 10:37:44 +0100

    Add files via upload

--- pagina prueba/pie.php
+++ pagina prueba/pie.php
@@ -5,9 +5,7 @@
                 <div class="row">
                     <div class="col-sm col-md item">
                         <h3>Derechos de Uso</h3>
-                        <p>La Ley Orgánica 15/1999, de 13 de diciembre, de Protección de Datos de Carácter Personal
-concede a los interesados la posibilidad de ejercer una serie de derechos relacionados con el
-tratamiento de sus datos personales.</p>
+                        <p><a href="https://www.iberley.es/temas/derechos-reales-uso-habitacion-60278">Derechos de uso y habitación</a> </p>
                     </div>
                     <div class="col-sm col-md item">
                         <h3>Patrocinadores</h3>
@@ -16,9 +14,7 @@ tratamiento de sus datos personales.</p>
                     </div>
                     <div class="col-sm col-md item">
                         <h3>Privacidad</h3>
-                        <p>Informamos a los usuarios del sitio web sobre su política respecto del tratamiento y
-protección de los datos de carácter personal de los usuarios y clientes que puedan ser
-recabados por la navegación o contratación de servicios a través de su sitio web.</p>
+                        <p><a href="https://ayudaleyprotecciondatos.es/2018/11/13/derecho-privacidad-espana/"> Derecho de la privacidad Española</a></p>
                     </div>
                     <div class="col-sm col-md item">
                         <h3>Autores</h3>
